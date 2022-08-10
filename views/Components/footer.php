a639dee83dc1c7d797c403a75032fa0a694d2172
Alan Tiller <alan23cowes@gmx.co.uk>
2022-08-10 20:51:44 +0100

    Changed footer format and added build version

--- views/Components/footer.php
+++ views/Components/footer.php
@@ -1,5 +1,12 @@
 <footer class="footer mt-auto py-3 bg-light">
     <div class="container">
-        <span class="text-muted">OpenSIPS Provisioning is licenced under MIT. Built and maintained by <a target="blank" href="https://github.com/alantiller/opensips-provisioning">Alan Tiller</a>.</span>
+        <div class="row">
+            <div class="col-md-6">
+                <span class="text-muted">OpenSIPS Provisioning is licenced under MIT. Build Version: 10.08.2022</span>
+            </div>
+            <div class="col-md-6">
+                <span class="text-muted">Built by Alan Tiller.</span>
+            </div>
+        </div>
     </div>
 </footer>
