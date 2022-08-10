93a85f222f46c0d144f9445234ccdeec02c4c0b6
Alan Tiller <alan23cowes@gmx.co.uk>
2022-08-10 20:54:23 +0100

    Fixed text alignment

--- views/Components/footer.php
+++ views/Components/footer.php
@@ -1,11 +1,11 @@
 <footer class="footer mt-auto py-3 bg-light">
     <div class="container">
         <div class="row">
-            <div class="col-md-6">
-                <span class="text-muted">OpenSIPS Provisioning is licenced under MIT. Build Version: 10.08.2022</span>
+            <div class="col-md-8">
+                <span class="text-muted d-block">OpenSIPS Provisioning is licenced under MIT. Build Version: 10.08.2022</span>
             </div>
-            <div class="col-md-6">
-                <span class="text-muted">Built by Alan Tiller.</span>
+            <div class="col-md-4">
+                <span class="text-muted d-block text-end">Designed and built by Alan Tiller.</span>
             </div>
         </div>
     </div>
