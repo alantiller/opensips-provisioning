4f27bd983bc2097bb3a1cea6ebbcb1269d6b3b55
Alan Tiller <alan23cowes@gmx.co.uk>
2022-08-11 21:04:05 +0100

    Add accountability to the audit logs

--- controllers/Audit/Audit.php
+++ controllers/Audit/Audit.php
@@ -26,25 +26,16 @@ class Audit
             // Get the request body
             $request_body = file_get_contents('php://input');
 
-            // Remove passwords from array
-            if (isset(json_decode($request_body, true)['password']))
-            {
-                $request_body = json_decode($request_body, true);
-                $request_body['password'] = "omitted";
-                $request_body = json_encode($request_body);
-            }
+            // Remove sensative fields
+            $request_body = self::omitted($request_body);
 
-            // Remove new password from array
-            if (isset(json_decode($request_body, true)['new_password']))
-            {
-                $request_body = json_decode($request_body, true);
-                $request_body['new_password'] = "omitted";
-                $request_body = json_encode($request_body);
-            }
+            // Get the user
+            $user = self::accountability();
 
             // Create the audit log
             $local->insert('osp_audit', [
                 'method' => $_SERVER['REQUEST_METHOD'],
+                'user' => $user,
                 'request_url' => $_SERVER['REQUEST_URI'],
                 'request_body' => $request_body,
                 'response_code' => http_response_code(),
@@ -54,9 +45,50 @@ class Audit
 
             return true;
         } 
-        catch (\Exception $error) 
+        catch (Exception $error) 
         {
             return false;
+        }
+    }
+
+    private static function omitted($request_body)
+    {
+        // Remove passwords from array
+        if (isset(json_decode($request_body, true)['password']))
+        {
+            $request_body = json_decode($request_body, true);
+            $request_body['password'] = "omitted";
+            $request_body = json_encode($request_body);
+        }
+
+        // Remove new password from array
+        if (isset(json_decode($request_body, true)['new_password']))
+        {
+            $request_body = json_decode($request_body, true);
+            $request_body['new_password'] = "omitted";
+            $request_body = json_encode($request_body);
+        }
+        
+        return $request_body;
+    }
+
+    private static function accountability()
+    {
+        try {
+            // Check if Token was passed in request
+            $token = (isset($_SERVER['HTTP_AUTHORIZATION']) ? str_replace('Bearer ', '', $_SERVER['HTTP_AUTHORIZATION']) : null);
+
+            // Check the authentication is correct
+            $check = \Auth\Auth::check($token);
+            if (!$check)
+            {
+                throw new \Exception("You are not authorised to access this endpoint, please provide an API Key.", 403);    
+            }
+            $user = \Auth\Auth::get($token);
+
+            return $user['id'];
+        } catch (Exception $error) {
+            return null;
         }
     }
 }
